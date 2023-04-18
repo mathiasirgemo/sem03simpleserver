8ab6d302e9f9785f497fd4058e68f3187a777501
mathiasirgemo <mathiasir@uia.no>
2023-04-18 13:10:32 +0000

    kryptert

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/mathiasirgemo/sem03
+
+go 1.18
+
+require github.com/uia-worker/is105sem03 v0.0.0-20230416140235-f3824ac768bb // indirect
--- tcp_server.go
+++ tcp_server.go
@@ -5,52 +5,54 @@ import (
 	"log"
 	"net"
 	"sync"
+	"github.com/uia-worker/is105sem03/mycrypt"
 )
 
 func main() {
-
-	var wg sync.WaitGroup
-
-	server, err := net.Listen("tcp", "172.17.0.3:")
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("bundet til %s", server.Addr().String())
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		for {
-			log.Println("før server.Accept() kallet")
-			conn, err := server.Accept()
-			if err != nil {
-				return
-			}
-			go func(c net.Conn) {
-				defer c.Close()
-				for {
-					buf := make([]byte, 1024)
-					n, err := c.Read(buf)
-					if err != nil {
-						if err != io.EOF {
-							log.Println(err)
-						}
-						return // fra for løkke
-					}
-					switch msg := string(buf[:n]); msg {
-  				        case "ping":
-						_, err = c.Write([]byte("pong"))
-					default:
-						_, err = c.Write(buf[:n])
-					}
-					if err != nil {
-						if err != io.EOF {
-							log.Println(err)
-						}
-						return // fra for løkke
-					}
-				}
-			}(conn)
-		}
-	}()
-	wg.Wait()
+    var wg sync.WaitGroup
+    server, err := net.Listen("tcp", "172.17.0.4:8788")
+    if err != nil {
+        log.Fatal(err)
+    }
+    log.Printf("bundet til %s", server.Addr().String())
+    wg.Add(1)
+    go func() {
+        defer wg.Done()
+        for {
+            log.Println("før server.Accept() kallet")
+            conn, err := server.Accept()
+            if err != nil {
+                return
+            }
+            go func(c net.Conn) {
+                defer c.Close()
+                for {
+                    buf := make([]byte, 1024)
+                    n, err := c.Read(buf)
+                    if err != nil {
+                        if err != io.EOF {
+                            log.Println(err)
+                        }
+                        return // fra for løkke
+                    }
+                    switch msg := string(buf[:n]); msg {
+                    case "ping":
+                        _, err = c.Write([]byte("pong"))
+                    default:
+                        dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+                        log.Println("Dekryptert melding: ", string(dekryptertMelding))
+                        _, err = c.Write([]byte(string(dekryptertMelding)))
+                    }
+                    if err != nil {
+                        if err != io.EOF {
+                            log.Println(err)
+                        }
+                        return // fra for løkke
+                    }
+                }
+            }(conn)
+        }
+    }()
+    wg.Wait()
 }
+
