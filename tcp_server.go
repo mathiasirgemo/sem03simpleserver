2731f96fda1255d7fba631c6e3173128a49d920f
mathiasirgemo <mathiasir@uia.no>
2023-04-22 11:45:26 +0000

    ferdig

--- tcp_server.go
+++ tcp_server.go
@@ -1,16 +1,24 @@
 package main
 
 import (
-	"io"
-	"log"
-	"net"
-	"sync"
-	"github.com/uia-worker/is105sem03/mycrypt"
+    "fmt"
+    "io"
+    "log"
+    "net"
+    "strconv"
+    "strings"
+    "sync"
+
+    "github.com/uia-worker/is105sem03/mycrypt"
 )
+
+func celsiusToFahrenheit(celsius float64) float64 {
+    return (celsius * 9 / 5) + 32
+}
 
 func main() {
     var wg sync.WaitGroup
-    server, err := net.Listen("tcp", "172.17.0.4:8788")
+    server, err := net.Listen("tcp", " 172.17.0.2:8148")
     if err != nil {
         log.Fatal(err)
     }
@@ -39,9 +47,25 @@ func main() {
                     case "ping":
                         _, err = c.Write([]byte("pong"))
                     default:
-                        dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
-                        log.Println("Dekryptert melding: ", string(dekryptertMelding))
-                        _, err = c.Write([]byte(string(dekryptertMelding)))
+                        if strings.HasPrefix(msg, "temperature:") {
+                            parts := strings.Split(msg, ":")
+                            if len(parts) != 2 {
+                                log.Println("Ugyldig temperaturmelding:", msg)
+                                continue
+                            }
+                            celsius, err := strconv.ParseFloat(parts[1], 64)
+                            if err != nil {
+                                log.Println("Kunne ikke parse Celsius-temperatur:", err)
+                                continue
+                            }
+                            fahrenheit := celsiusToFahrenheit(celsius)
+                            response := fmt.Sprintf("temperature:%.1f", fahrenheit)
+                            _, err = c.Write([]byte(response))
+                        } else {
+                            dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+                            log.Println("Dekryptert melding: ", string(dekryptertMelding))
+                            _, err = c.Write([]byte(string(dekryptertMelding)))
+                        }
                     }
                     if err != nil {
                         if err != io.EOF {
@@ -55,4 +79,3 @@ func main() {
     }()
     wg.Wait()
 }
-
